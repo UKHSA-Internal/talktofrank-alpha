f1be56b4238c955da76b61783d7e7a0fb06d039e
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-02 15:41:19 +0100

    Linting

--- app/shared/components/Typography/component.jsx
+++ app/shared/components/Typography/component.jsx
@@ -3,32 +3,20 @@ import React from 'react'
 const Typography = props => {
   return (
     <main className='container container-fluid'>
-
       <h1>This is a chunk of h1</h1>
       <p className='h1'>This is a chunk of h1 sized paragraph text that is being churned out</p>
-
-
       <hr className='divider mt-5 mb-5'/>
-
       <h2>This is an actual h2</h2>
       <p className='h2'>This is a chunk of h2 paragraph text - indistinguishable except perhaps the bottom margin</p>
-
       <hr className='divider mt-5 mb-5'/>
-
       <h3>This is an actual h3</h3>
       <p className='h3'>This is a chunk of h3 paragraph text - indistinguishable except perhaps the bottom margin</p>
-
       <hr className='divider mt-5 mb-5'/>
-
       <h4>This is an actual h4 - time to test it out in the format below....</h4>
       <p className='h4'>...which is a chunk of h4 sized paragraph text - super</p>
-
       <hr className='divider mt-5 mb-5'/>
-
       <h5>This is an actual h5 - time to test it out in the format below....</h5>
       <p className='h5'>...which is a chunk of h5 sized paragraph text - super</p>
-
-
       <hr className='divider mt-5 mb-5'/>
       <ul>
         <li>Something</li>
@@ -37,9 +25,7 @@ const Typography = props => {
         <li>Something</li>
         <li>Something</li>
       </ul>
-
       <hr className='divider mt-5 mb-5'/>
-
       <ul className="nav breadcrumb">
         <li className='breadcrumb__item'>
           <a href='#' className='breadcrumb__link'>Amount</a>
@@ -54,7 +40,6 @@ const Typography = props => {
           <a href='#' className='breadcrumb__link'>Confirm</a>
         </li>
       </ul>
-
     </main>
   )
 }
