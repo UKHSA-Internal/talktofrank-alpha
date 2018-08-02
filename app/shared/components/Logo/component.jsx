95ddf36e188e07aecc81642a4fd212448dc2763d
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-02 16:30:07 +0100

    Added logo and svg

---
+++ app/shared/components/Logo/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <object data='/ui/svg/log-frank.svg' type='image/svg+xml'>
+      <img src='yourfallback.jpg' />
+    </object>
+  )
+}
+
+export default Logo
