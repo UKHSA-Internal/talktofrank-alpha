8d4a77d403e7204a2dae073968c14c3264f9b554
Jon <jon@kode.uk.com>
2018-08-07 20:02:37 +0100

    Debugging

--- app/config-loader.js
+++ app/config-loader.js
@@ -28,5 +28,5 @@ envConfig = loadConfig('/../config.' + env + '.yaml')
 baseConfig.buildTimestamp = Date.now()
 
 var config = merge.recursive(true, baseConfig, envConfig)
-
+console.log(config)
 export { config }
