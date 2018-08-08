2141a134513bf70b3573a0e53f55214d3a5091a6
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-08 10:26:38 +0100

    Kitting out the drug page with the correct header and new logo

--- app/shared/components/DrugList/component.jsx
+++ app/shared/components/DrugList/component.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Masthead from '../Masthead/component.jsx'
+import Grid from '../Grid/component.jsx'
+import GridCol from '../GridCol/component.jsx'
 
 const DrugList = props => {
   return (
-    <div>
+    <div className='main-wrapper'>
+      <Masthead />
       <h1>A-Z</h1>
-      <ul>
-        {props.list && props.list.map((item, key) => (
-          <li key={`drug-${key}`}>
-            <Link to={item.slug}>{item.name}</Link>
-          </li>
-        ))}
-      </ul>
+      <Grid>
+        <GridCol className='col-12 col-sm-8'>
+          <ul>
+            {props.list && props.list.map((item, key) => (
+              <li key={`drug-${key}`}>
+                <Link to={item.slug}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </GridCol>
+      </Grid>
     </div>
   )
 }
