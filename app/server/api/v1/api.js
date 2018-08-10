73e09c13b0c3a6e9e16fb1056dd45897212608b5
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-10 16:22:57 +0100

    Filtering numbers out of drug list
    I filtered them ans concatenated them at the end but when I mapped them for rendering it stuffed them back in at the front again. *sigh*

--- app/server/api/v1/api.js
+++ app/server/api/v1/api.js
@@ -167,8 +167,19 @@ router.get('/drugList', (req, res, next) => {
         // })
       })
 
+      let numbers = []
       response.list = sortBy(response.list, (item) => (item.name))
+        .filter(v => {
+          if (!isNaN(parseFloat(v.name))) {
+            numbers.push(v)
+            return
+          }
+        return isNaN(parseFloat(v.name))
+      })
 
+      // @joel - commenting this out as the filtering / mapping at the component
+      // level messes the order up again and ignores that the numers come last : /
+      // response.list = response.list.concat(numbers)
       res.send(response)
     })
   } catch (e) {
