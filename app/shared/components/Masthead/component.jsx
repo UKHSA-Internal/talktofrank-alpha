a3f5a0af65310616e262da44012632902c935497
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-10 11:47:13 +0100

    Navigation amends

--- app/shared/components/Masthead/component.jsx
+++ app/shared/components/Masthead/component.jsx
@@ -43,10 +43,13 @@ export default class Masthead extends React.PureComponent {
                   <a className='nav-link' href='#'>Drugs news</a>
                 </li>
                 <li className='nav-item active'>
-                  <a className='nav-link' href='#'>Get help</a>
+                  <a className='nav-link' href='#'>Help for you</a>
                 </li>
                 <li className='nav-item active'>
-                  <a className='nav-link' href='#'>Contact</a>
+                  <a className='nav-link' href='#'>Help for others</a>
+                </li>
+                <li className='nav-item active'>
+                  <a className='nav-link' href='#'>Contact Frank</a>
                 </li>
               </ul>
             </nav>
