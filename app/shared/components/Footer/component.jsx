e7701e177f5f85349cd066be5b5ef5fe1468c7fd
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-10 09:07:20 +0100

    Footer button styles and size tweaks

--- app/shared/components/Footer/component.jsx
+++ app/shared/components/Footer/component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import Grid from '../Grid/component.jsx'
 import GridCol from '../GridCol/component.jsx'
 import Button from '../Button/component.jsx'
@@ -6,16 +6,16 @@ import Button from '../Button/component.jsx'
 const Footer = props => {
   return (
     <footer className='footer'>
-      <div className='main-wrapper text-center'>
+      <div className='content-wrapper text-center'>
         <Grid>
           <GridCol className='col-12 col-sm-6'>
-            <Button className='btn--primary raised' label='Call us: 0300 123 6600'/>
+            <Button className='btn--primary btn--large btn--raised' label='Call us: 0300 123 6600'/>
           </GridCol>
           <GridCol className='col-12 col-sm-6'>
-
+            <p>Email us: <a href='mailto:frank@talktofrank.com'></a></p>
+            <p>Text us: <a href='tel:82111'></a></p>
           </GridCol>
         </Grid>
-        Email us: frank@talktofrank.com
       </div>
     </footer>
   )
