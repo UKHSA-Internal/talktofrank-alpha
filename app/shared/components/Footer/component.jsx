05890b92ab89b2f081a542707b92ebdeccb53253
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-10 08:40:08 +0100

    Adding footer element

---
+++ app/shared/components/Footer/component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = props => {
+
+  return (
+    <footer className='footer pt-5 pb-5'>
+      <div className='container text-center'>
+
+      </div>
+    </footer>
+  )
+}
+export default Footer
