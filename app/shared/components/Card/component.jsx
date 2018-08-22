481a1e7c2bd8c1a7f69fab2b35122a9d23502c21
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-22 09:51:21 +0100

    Heading tinkering

--- app/shared/components/Card/component.jsx
+++ app/shared/components/Card/component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Svg from '../Svg/component.jsx'
+import Heading from '../Heading/component.jsx'
 
 const Card = props => {
   return (
@@ -8,8 +9,8 @@ const Card = props => {
         <Svg url={props.imageUrl} alt='' className='card-img-top'/>
         <div className='card-body'>
           {props.category && <small className='muted'>{props.category}</small>}
-          <h3 className='card-title h3 spacing-top--flush'>{props.title}</h3>
-          <p className='card-text'>{props.content}</p>
+          <Heading className='card-title h3 spacing-top--flush' text={props.title}/>
+          {props.content && <p className='card-text'>{props.content}</p>}
         </div>
       </a>
     </div>
