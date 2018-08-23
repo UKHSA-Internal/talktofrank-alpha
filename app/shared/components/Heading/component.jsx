d70b287e3d6ccb2a2b97cefa0db4988ac47fb940
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-08-23 07:43:11 +0100

    Adding a merging function to supply extra class names

--- app/shared/components/Heading/component.jsx
+++ app/shared/components/Heading/component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import classNames from 'classnames'
 
 const Heading = props => {
   const Tag = `${props.type || 'h2'}`
-
+  let classes = classNames(props.className, props.modifiers)
   return (
     <Tag className={props.className} dangerouslySetInnerHTML={{__html: props.text}}></Tag>
   )
