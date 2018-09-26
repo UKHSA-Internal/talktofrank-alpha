24606f7d9df0f2603787fc8cfe08661079aca6e7
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-09-26 15:10:50 +0100

    Noting refactor

--- app/shared/components/GoogleAnalytics/component.jsx
+++ app/shared/components/GoogleAnalytics/component.jsx
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga'
 const GA = props => {
   if (typeof window !== 'undefined') {
     if (!window.ga) {
-      ReactGA.initialize('UA-129232-18')
+      ReactGA.initialize('UA-129232-18') // @todo - this would be better coming from the config.yaml
     }
     ReactGA.pageview(window.location.pathname + window.location.search)
   }
