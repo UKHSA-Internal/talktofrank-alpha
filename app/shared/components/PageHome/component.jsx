bed3d50a431721a421041a3733c2ab3a32d6eb12
Joel Mitchell <joel.mitchell@cxpartners.co.uk>
2018-09-26 13:23:13 +0100

    Adding items / events to page home for events

--- app/shared/components/PageHome/component.jsx
+++ app/shared/components/PageHome/component.jsx
@@ -10,88 +10,113 @@ import Footer from '../Footer/component.jsx'
 import CardDeck from '../CardDeck/component.jsx'
 import LinkDeck from '../LinkDeck/component.jsx'
 import GA from '../GoogleAnalytics/component.jsx'
+import ReactGA from 'react-ga'
+import Button from '../Button/component.jsx'
 
 // @todo @refactor @joel - haul this out of here and into a fixture / Contentful
-const PageHome = props => {
+export default class PageHome extends React.PureComponent  {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-  const teasers = [{
-    modifiers: '',
-    url: '/',
-    title: '5 tips for a safe festival experience',
-    category: 'News',
-    imageUrl: '/ui/img/content/news-1.jpg'
-  },
-  {
-    modifiers: '',
-    url: '/',
-    title: 'The total truth about legal highs',
-    category: 'News',
-    imageUrl: '/ui/img/content/news-2.jpg'
-  },
-  {
-    modifiers: '',
-    url: '/',
-    title: 'What are smart drugs and are they legal? ',
-    category: 'News',
-    imageUrl: '/ui/img/content/news-3.jpg'
-  }]
+  handleClick() {
+    ReactGA.event({
+      category: 'Navigation',
+      action: 'Clicked Link',
+      label: 'this is a label'
+    })
+  }
 
-  const cta = [{
-    modifiers: '',
-    url: '/',
-    title: 'Help for you'
-  },
-  {
-    modifiers: '',
-    url: '/',
-    title: 'Help for others'
-  },
-  {
-    modifiers: '',
-    url: '/drug',
-    title: 'A-Z of drugs'
-  }]
+  componentDidMount() {
+    ReactGA.timing({
+      category: 'React libraries - client side',
+      variable: 'load',
+      value: 20, // in milliseconds
+      label: 'React bundles'
+    })
+    console.log('I mounted! Here are my props');
+  }
 
-  return (
-    <React.Fragment>
-      <Masthead/>
-      <Main className='homepage'>
-        <section className='panel panel--padding-large has-bg bg-pills'>
+  render () {
+    const teasers = [{
+      modifiers: '',
+      url: '/',
+      title: '5 tips for a safe festival experience',
+      category: 'News',
+      imageUrl: '/ui/img/content/news-1.jpg'
+    },
+    {
+      modifiers: '',
+      url: '/',
+      title: 'The total truth about legal highs',
+      category: 'News',
+      imageUrl: '/ui/img/content/news-2.jpg'
+    },
+    {
+      modifiers: '',
+      url: '/',
+      title: 'What are smart drugs and are they legal? ',
+      category: 'News',
+      imageUrl: '/ui/img/content/news-3.jpg'
+    }]
+
+    const cta = [{
+      modifiers: '',
+      url: '/',
+      title: 'Help for you'
+    },
+    {
+      modifiers: '',
+      url: '/',
+      title: 'Help for others'
+    },
+    {
+      modifiers: '',
+      url: '/drug',
+      title: 'A-Z of drugs'
+    }]
+
+    return (
+      <React.Fragment>
+        <Masthead/>
+        <Main className='homepage'>
+          <section className='panel panel--padding-large has-bg bg-pills'>
+            <Grid>
+              <GridCol className='col-12 col-md-6'>
+                <Heading text='Honest information about drugs' type='h1' modifiers='display-3 spacing-top--flush'/>
+              </GridCol>
+            </Grid>
+          </section>
+          <section className='panel panel--padding-large panel--pink'>
+            <Form>
+              <FormGroupAutocomplete
+                button='true'
+                modifiers='form-control--search'
+                className='input-group-autocomplete--inverse'
+                id='search-a-z'
+                label='Search for any drug'
+                showContent
+                titleClass='h4'
+                placeholder='Enter a drug name (e.g. Mandy, Cocaine, Weed)'
+                resultsId='body-results'
+              />
+            </Form>
+            <Button clickHandler={this.handleClick}>Click me to send an event</Button>
+          </section>
           <Grid>
-            <GridCol className='col-12 col-md-6'>
-              <Heading text='Honest information about drugs' type='h1' modifiers='display-3 spacing-top--flush'/>
+            <GridCol className='col-12'>
+              <Heading text='News articles' className='sr-only'/>
+              <CardDeck teasers={teasers} className='spacing--large card-deck--compact'/>
+            </GridCol>
+            <GridCol className='col-12'>
+              <LinkDeck teasers={cta} className='spacing-bottom--large'/>
             </GridCol>
           </Grid>
-        </section>
-        <section className='panel panel--padding-large panel--pink'>
-          <Form>
-            <FormGroupAutocomplete
-              button='true'
-              modifiers='form-control--search'
-              className='input-group-autocomplete--inverse'
-              id='search-a-z'
-              label='Search for any drug'
-              showContent
-              titleClass='h4'
-              placeholder='Enter a drug name (e.g. Mandy, Cocaine, Weed)'
-              resultsId='body-results'
-            />
-          </Form>
-        </section>
-        <Grid>
-          <GridCol className='col-12'>
-            <Heading text='News articles' className='sr-only'/>
-            <CardDeck teasers={teasers} className='spacing--large card-deck--compact'/>
-          </GridCol>
-          <GridCol className='col-12'>
-            <LinkDeck teasers={cta} className='spacing-bottom--large'/>
-          </GridCol>
-        </Grid>
-      </Main>
-      <Footer />
-      <GA/>
-    </React.Fragment>
-  )
+        </Main>
+        <Footer />
+        <GA/>
+      </React.Fragment>
+    )
+  }
 }
-
-export default PageHome
